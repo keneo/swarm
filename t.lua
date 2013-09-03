6f632ef33e953cac32b7111ab904f9b8b5f45c0d
bartek <bartek@szabat.com>
2013-09-04 00:58:23 +0200

    Update t.lua

--- t.lua
+++ t.lua
@@ -1,3 +1,3 @@
 t=turtle
-d={["f"]=t.forward,["u"]=t.up,["d"]=t.down,["b"]=t.back,["l"]=t.turnLeft,["r"]=t.turnRight,["F"]=t.dig,["U"]=t.digUp,["D"]=t.digDown}
+d={["f"]=t.forward,["u"]=t.up,["d"]=t.down,["b"]=t.back,["l"]=t.turnLeft,["r"]=t.turnRight,["F"]=t.dig,["U"]=t.digUp,["D"]=t.digDown,["P"]=t.place,["_"]=function()end}
 ({...})[1]:gsub(".",function(c)d[c]()end)
