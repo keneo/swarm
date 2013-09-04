b59a11d81a1032b734309c195b092b92694cfcce
bartek <bartek@szabat.com>
2013-09-04 19:40:53 +0200

    Create t.api.lua

---
+++ t.api.lua
@@ -0,0 +1,30 @@
+function t(code)
+  local t=turtle
+  d={
+    ["f"]=t.forward,
+    ["u"]=t.up,
+    ["d"]=t.down,
+    ["b"]=t.back,
+    ["l"]=t.turnLeft,
+    ["r"]=t.turnRight,
+    ["F"]=t.dig,
+    ["U"]=t.digUp,
+    ["D"]=t.digDown,
+    ["P"]=t.place,
+    ["_"]=function()end
+  }
+  
+  ({...})[1]:gsub(".",
+                      function(c)
+                        if c:find("%d")
+                          then 
+                            t.select(c)
+                          else 
+                            d[c]()
+                        end
+  end)
+end
+
+function stairsUp(k)
+  t(string.rep("uUFfUFD",k))
+end
