dc621957b8ac2cd1a1a855df8a18515bb67ba8ba
bartek <bartek@szabat.com>
2013-09-04 17:51:19 +0200

    Update t.lua

--- t.lua
+++ t.lua
@@ -1,3 +1,6 @@
 t=turtle
 d={["f"]=t.forward,["u"]=t.up,["d"]=t.down,["b"]=t.back,["l"]=t.turnLeft,["r"]=t.turnRight,["F"]=t.dig,["U"]=t.digUp,["D"]=t.digDown,["P"]=t.place,["_"]=function()end}
-({...})[1]:gsub(".",function(c)d[c]()end)
+({...})[1]:gsub(".",function(c)
+if c:find("%d") then t.select(c)
+else d[c]()
+end)
