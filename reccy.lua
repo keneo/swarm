3914f119495c00509f250606786404717d92a02a
bartek <bartek@szabat.com>
2013-10-08 22:52:45 +0200

    Update reccy.lua

--- reccy.lua
+++ reccy.lua
@@ -1,13 +1,40 @@
 -- recursive mining bot attempt
 
+function roadclear()
+  turtle.dig()
+end
+
+function lookY(name,c,m,d)
+  if (worth(c)) then
+    while (not m()) do 
+      if (not d()) then
+        write ("cancel dig look y "..name)
+        return
+      end
+    end
+  end
+end
+
+function lookaround()
+  lookY("up",turtle.compareUp,turtle.up,turtle.digUp)
+  lookY("down",turtle.compareDown,turtle.down,turtle.digDown)
+end
+
 function moveforward(n)
   if (n==0) then return end
   
-  turtle.forward() ? 
+  while (not turtle.forward()) do
+    if (not turtle.dig()) then
+      write ("cancel dig forw "..n)
+      return
+    end
+  end
+  
+  lookaround()
+  moveforward(n-1)
+  turtle.back()
 end
 
-function start()
-  moveforward(10)
-  forward() ? {put(forward) ... } : (dig(forward)||(deadend()) )
-end
+moveforward(10)
+--  forward() ? {put(forward) ... } : (dig(forward)||(deadend()) )
 
