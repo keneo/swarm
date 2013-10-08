ef73cb8ab9296cc905e03426a674b84ba2cafc6e
bartek <bartek@szabat.com>
2013-10-08 21:47:25 +0200

    Update reccy.lua

--- reccy.lua
+++ reccy.lua
@@ -1,4 +1,13 @@
-function start()
+-- recursive mining bot attempt
 
+function moveforward(n)
+  if (n==0) then return end
+  
+  turtle.forward() ? 
+end
+
+function start()
+  moveforward(10)
+  forward() ? {put(forward) ... } : (dig(forward)||(deadend()) )
 end
 
