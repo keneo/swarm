e74d7ec60a65489d5cc3d1f336a88194a07a585b
bartek <bartek@szabat.com>
2013-10-08 23:18:16 +0200

    Update reccy.lua

--- reccy.lua
+++ reccy.lua
@@ -1,23 +1,36 @@
 -- recursive mining bot attempt
 
-function roadclear()
-  turtle.dig()
+function worth(c)
+  for p=1,3 do --unintresting
+    turtle.select(p)
+    if (c()) then
+      return false
+    end
+  end
+  return true
 end
 
-function lookY(name,c,m,d)
-  if (worth(c)) then
-    while (not m()) do 
-      if (not d()) then
+function lookY(name,compare,move,dig,moveBack)
+  if (worth(compare)) then
+    repeat
+      if (not dig()) then
         write ("cancel dig look y "..name)
         return
       end
-    end
+    until not move()
+    lookaround()
+    moveBack()
   end
 end
 
 function lookaround()
-  lookY("up",turtle.compareUp,turtle.up,turtle.digUp)
-  lookY("down",turtle.compareDown,turtle.down,turtle.digDown)
+  lookY("up",turtle.compareUp,turtle.up,turtle.digUp,turtle.down)
+  lookY("down",turtle.compareDown,turtle.down,turtle.digDown,turtle.up)
+  
+  for s=1,4 do
+    lookY("forw",turtle.compare,turtle.forward,turtle.dig,turtle.back)
+    turtle.turnRight()
+  end
 end
 
 function moveforward(n)
